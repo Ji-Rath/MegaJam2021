7eb5c5373bb1590a5719bcb1cb2fe45149f87844
Ji-Rath <garethaguilar@gmail.com>
2021-08-22 15:11:21 -0500

    Add initial project files

---
+++ Source/MegaJam2021/MegaJam2021.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MegaJam2021.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MegaJam2021, "MegaJam2021" );
---
+++ Source/MegaJam2021/MegaJam2021GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MegaJam2021GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEGAJAM2021_API AMegaJam2021GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
