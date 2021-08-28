d158f56274af13d0265db438ee14f33a81ba8100
Ji-Rath <garethaguilar@gmail.com>
2021-08-28 16:58:36 -0500

    Implement AI through EnemyController class
    Includes sight stimulus response, state changing, and forgetting

--- Source/MegaJam2021/EnemyController.cpp
+++ Source/MegaJam2021/EnemyController.cpp
@@ -4,24 +4,77 @@
 #include "EnemyController.h"
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AIPerceptionTypes.h"
+#include "Perception/AISense_Sight.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 AEnemyController::AEnemyController()
 {
-	PerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("PerceptionComponent"));
+	AIPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("PerceptionComponent"));
 }
 
 void AEnemyController::BeginPlay()
 {
+	Super::BeginPlay();
+
 	// Bind perception update function to delegate
 	GetPerceptionComponent()->OnTargetPerceptionUpdated.AddDynamic(this, &AEnemyController::PerceptionUpdated);
 }
 
 void AEnemyController::Tick(float DeltaTime)
 {
-
+	Super::Tick(DeltaTime);
 }
 
 void AEnemyController::PerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
+	const FAISenseID SenseID = Stimulus.Type;
+	const FAISenseID SightID = UAISense::GetSenseID(UAISense_Sight::StaticClass());
+	
+	/** Sight stimulus */
+	if (SenseID == SightID)
+	{
+		if (Stimulus.WasSuccessfullySensed())
+		{
+			PlayerReference = Actor;
+			SetState(Chasing);
+			GetBlackboardComponent()->SetValueAsObject(BBPlayerReference, Actor);
 
+			// Clear timer that would cause the enemy to automatically search
+			if (GetWorldTimerManager().IsTimerActive(ForgetTargetTimer))
+			{
+				GetWorldTimerManager().ClearTimer(ForgetTargetTimer);
+			}
+		}
+		else
+		{
+			FTimerDelegate TimerDelegate;
+			TimerDelegate.BindUFunction(this, FName("SetState"), Searching);
+			GetWorldTimerManager().SetTimer(ForgetTargetTimer, TimerDelegate, ForgetTargetTime, false);
+		}
+		
+	}
+}
+
+void AEnemyController::SetState(EEnemyState NewState)
+{
+	EnemyState = NewState;
+	GetBlackboardComponent()->SetValueAsEnum(BBEnemyState, (uint8) EnemyState);
+
+	// Update search location
+	if (NewState == Searching && PlayerReference)
+	{
+		GetBlackboardComponent()->SetValueAsVector(BBSearchLocation, PlayerReference->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector(BBDestination, PlayerReference->GetActorLocation());
+
+		FTimerDelegate TimerDelegate;
+		TimerDelegate.BindUFunction(this, FName("SetState"), Idle);
+		GetWorldTimerManager().SetTimer(ForgetTargetTimer, TimerDelegate, StopSearchingTime, false);
+	}
+
+	OnStateUpdated.Broadcast(NewState);
+}
+
+UAIPerceptionComponent* AEnemyController::GetPerceptionComponent()
+{
+	return AIPerceptionComponent;
 }
--- Source/MegaJam2021/EnemyController.h
+++ Source/MegaJam2021/EnemyController.h
@@ -9,6 +9,16 @@
 
 class UAIPerceptionComponent;
 
+UENUM()
+enum EEnemyState
+{
+	Idle,
+	Searching,
+	Chasing
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FStateUpdated, EEnemyState, EnemyState);
+
 /**
  * 
  */
@@ -24,6 +34,57 @@ public:
 
 	void Tick(float DeltaTime) override;
 
+	/** Called whenever AI Perception receives a stimulus */
 	UFUNCTION()
 	void PerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+
+	/** The current state of the enemy */
+	UPROPERTY(EditAnywhere)
+	TEnumAsByte<EEnemyState> EnemyState;
+
+	/** String reference to BB Vector. Intended to guide AI to stimulus */
+	UPROPERTY(EditDefaultsOnly)
+	FName BBDestination;
+
+	/** String reference to BB Vector. Intended for searching an area */
+	UPROPERTY(EditDefaultsOnly)
+	FName BBSearchLocation;
+
+	/** String reference to BB enum. Used to manage AI state */
+	UPROPERTY(EditDefaultsOnly)
+	FName BBEnemyState;
+
+	/** String reference to BB enum. Used to manage AI state */
+	UPROPERTY(EditDefaultsOnly)
+	FName BBPlayerReference;
+
+	/** Distance required from player to begin chasing */
+	UPROPERTY(EditDefaultsOnly)
+	float AlertDistance = 500.f;
+
+	UFUNCTION()
+	void SetState(EEnemyState NewState);
+
+	/** Time it takes to 'lose' the target and begin searching */
+	UPROPERTY(EditDefaultsOnly)
+	float ForgetTargetTime = 5.f;
+
+	/** Time it takes for the enemy to give up on searching for the player */
+	UPROPERTY(EditDefaultsOnly)
+	float StopSearchingTime = 10.f;
+
+	UAIPerceptionComponent* GetPerceptionComponent() override;
+
+	UPROPERTY(VisibleAnywhere)
+	UAIPerceptionComponent* AIPerceptionComponent;
+
+	UPROPERTY(BlueprintAssignable)
+	FStateUpdated OnStateUpdated;
+
+private:
+	UPROPERTY()
+	AActor* PlayerReference;
+
+	UPROPERTY()
+	FTimerHandle ForgetTargetTimer;
 };
